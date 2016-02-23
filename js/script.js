636279db226ecdffa51fd928377a4acb23521e64
Yanqing Liu <touch3247@gmail.com>
2016-02-23 15:23:26 -0800

    third commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,37 @@
 $(document).ready(function(){
 	$("#qform").submit(function(e){
-		console.log("bk");
-
 		e.preventDefault();
 		console.log("submitted");
+
+		var ans = $("input:radio[name='answer']:checked").val();
+		console.log(ans);
+
 	});
 
-	$("#ntBt").click(function(e){
-		console.log("bk1");
-		var option = $("input[name='answer']").val();
-		console.log(option);
-	})
 })
 
-function nextQuestion(){
+var questionList = [{
+		q: "How many oscars did the Titanic movie got?
+Eleven",
+		a: ["7", "9", "11", "13"]
+	},
+	{
+		q: "What is the name of the prison in the film The Rock?",
+		a: ["Alcatraz", "b", "c", "d"]
+	},
+	{
+		q: "What is the name of the little dragon in the animated movie Mulan?",
+		a: ["Mushu", "b", "c", "d"]
+	},
+	{
+		q: "What is the name of the bald commander of the Enterprise in Star Trek?",
+		a: ["Captain Picard", "b", "c", "d"]
+	},
+	{
+		q: "What is the name of Steven Spielberg s black-and-white-film about the second world war?",
+		a: ["Schindler s List", "b", "c", "d"]
+	}];
 
+function showNextQuestion(){
+	$("p.head").text();
 }
